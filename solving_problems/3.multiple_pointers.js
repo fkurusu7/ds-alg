bb8cc10c8ddf4cb1dec992b2fdcfd58a2a804d42
Fernando Cruz <f.douzez@gmail.com>
2024-06-28 23:44:27 -0600

    add another example for multiple pointers

--- solving_problems/3.multiple_pointers.js
+++ solving_problems/3.multiple_pointers.js
@@ -31,3 +31,27 @@ function sumZero(arr) {
 
 console.log(sumZero([-3, -2, 0, 3, 6]));
 console.log(sumZero([-3, -2, 0, 5, 6]));
+
+console.log("******** EXAMPLE ********");
+console.log(`Implement a function called countUniqueValues,
+  which accepts a sorted array, and counts the unique values in the array.
+  There can be negative numbers in the array, but it always be sorted.`);
+
+function countUniqueValuesNaive(arr) {
+  if (arr.length === 0) return 0;
+
+  let currValue = null;
+  let counter = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (currValue === arr[i]) continue;
+    counter++;
+    currValue = arr[i];
+  }
+  return counter;
+}
+console.log(countUniqueValuesNaive([1, 1, 1, 1, 1, 5])); // ==> 2
+console.log(countUniqueValuesNaive([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // ==> 7
+console.log(countUniqueValuesNaive([])); // ==> 0
+console.log(countUniqueValuesNaive([-2, -2, -1, 0, 1])); // ==> 4
+console.log(countUniqueValuesNaive([-2, -2, -1, 0, 1, 3, 3, 5])); // ==> 6
