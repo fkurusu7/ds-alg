d2dd1cfa2978a018b124302bcf32682858c603b0
Fernando Cruz <f.douzez@gmail.com>
2024-07-10 15:05:03 -0600

    add new codewars 5kyu challenge

--- solving_problems/level_easy_random_challenges.js
+++ solving_problems/level_easy_random_challenges.js
@@ -139,3 +139,32 @@ console.log(splitStringinPairs("abc"));
 // abc abc_ ["ab", "c_"]
 // abcd abcd_ ["ab", "cd"]
 console.log(splitStringinPairs("abcd"));
+
+console.log(`Write a function, which takes a non-negative integer (seconds) as input 
+  and returns the time in a human-readable format (HH:MM:SS)
+    HH = hours, padded to 2 digits, range: 00 - 99
+    MM = minutes, padded to 2 digits, range: 00 - 59
+    SS = seconds, padded to 2 digits, range: 00 - 59
+  The maximum time never exceeds 359999 (99:59:59)`);
+
+function humanReadable(seconds) {
+  const hours = parseInt(seconds / 60 / 60);
+  const mins = parseInt((seconds / 60) % 60);
+  const secs = parseInt(seconds % 60);
+
+  const finalHours = hours <= 9 ? "0" + hours : hours;
+  const finalMins = mins <= 9 ? "0" + mins : mins;
+  const finalSecs = secs <= 9 ? "0" + secs : secs;
+
+  return `${finalHours}:${finalMins}:${finalSecs}`;
+}
+
+console.log(humanReadable(0)); //     ==>  00:00:00
+console.log(humanReadable(59)); //    ==>  00:00:59
+console.log(humanReadable(60)); //    ==>  00:01:00
+console.log(humanReadable(90)); //    ==>  00:01:30
+console.log(humanReadable(3599)); //  ==>  00:59:59
+console.log(humanReadable(3600)); //  ==>  01:00:00
+console.log(humanReadable(45296)); // ==>  12:34:56
+console.log(humanReadable(86399)); // ==>  23:59:59
+console.log(humanReadable(86400)); // ==>  24:00:00
