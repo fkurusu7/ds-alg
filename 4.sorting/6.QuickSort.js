9f30816efbd8dbd075410a990e57a691bdd95e6d
fkurusu7 <f.douzez@gmail.com>
2024-07-12 23:42:08 -0600

    add quick sort file

---
+++ 4.sorting/6.QuickSort.js
@@ -0,0 +1,12 @@
+console.log("*****************************************");
+console.log("************** MERGE SORT ***************");
+console.log("************** O(n log n) ***************");
+console.log("*****************************************");
+
+console.log(`*** Exploits the fact that arrays of 
+    0 and 1 are always sorted`);
+console.log(`*** Works by selecting one element called "PIVOT"
+    and finding the index where the pivot should end up in 
+    the sorted array.`);
+console.log(`*** Once the pivot is positioned appropriately, QS 
+    can be applied on either side of the pivot`);
