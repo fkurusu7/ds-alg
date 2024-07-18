84dcee116b986843f3f49faf5765e17646883713
fkurusu7 <f.douzez@gmail.com>
2024-07-17 23:43:53 -0600

    add push methos to singly linked list undertading it!

--- 5.data_structures/2.singly_linked_list.js
+++ 5.data_structures/2.singly_linked_list.js
@@ -18,9 +18,10 @@ console.log("*** Random access is not allowed");
 console.log("piece of data");
 console.log("reference to next node - next");
 
+/** Class Node ==> props: val */
 class Node {
-  constructor(value) {
-    this.value = value;
+  constructor(val) {
+    this.val = val;
     this.next = null;
   }
 }
@@ -32,38 +33,28 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
-  /* PSEUDOCODE for inserting 
-    - This function should accept a value
-    - Create a new node using the value passed to the function
-    - if there is no HEAD property on the list, 
-      set the head and tail to be the newly created node
-    - Otherwise set the next property on the tail to be the new node 
-      and set the tail property on the list to be the newly created one.
-    - Increment the length by one
+  /* PSEUDOCODE for PUSH (inserting) 
+  - This function should accept a value
+  - Create a new node using the value passed to the function
+  - if there is no HEAD property on the list, 
+  set the head and tail to be the newly created node
+  - Otherwise set the next property on the tail to be the new node 
+  and set the tail property on the list to be the newly created one.
+  - Increment the length by one
   */
-  push(value) {
-    const newNode = new Node(value);
+  push(val) {
+    const newNode = new Node(val);
+    console.log(newNode);
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
     } else {
       this.tail.next = newNode;
-
       this.tail = newNode;
     }
+
     this.length++;
     return this;
-  }
-  /* PSEUDOCODE for removing 
-    - If there are no nodes in the list, return undefined
-    - Loop through the list until you reach the tail
-    - Set the next property of the 2nd to last node to be null
-    - Set the tail to be the 2nd to last node
-    - Decrement the length of the list by 1
-    - Return the value of the node removed
-  */
-  pop() {
-    if (!this.head) return undefined;
   }
 }
 
@@ -76,3 +67,12 @@ list.push(1);
 console.log(list);
 list.push(10);
 console.log(list);
+
+/* PSEUDOCODE for POP (removing)
+    - If there are no nodes in the list, return undefined
+    - Loop through the list until you reach the tail
+    - Set the next property of the 2nd to last node to be null
+    - Set the tail to be the 2nd to last node
+    - Decrement the length of the list by 1
+    - Return the value of the node removed
+  */
