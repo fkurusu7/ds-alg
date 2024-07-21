7a6d358c7982029d323920cc9e374b4e8c3115f9
fkurusu7 <f.douzez@gmail.com>
2024-07-20 22:54:00 -0600

    add new random challenge from codewars

--- solving_problems/level_easy_random_challenges.js
+++ solving_problems/level_easy_random_challenges.js
@@ -168,3 +168,29 @@ console.log(humanReadable(3600)); //  ==>  01:00:00
 console.log(humanReadable(45296)); // ==>  12:34:56
 console.log(humanReadable(86399)); // ==>  23:59:59
 console.log(humanReadable(86400)); // ==>  24:00:00
+
+/** A pangram is a sentence that contains every single letter 
+ * of the alphabet at least once. For example, the sentence 
+ * "The quick brown fox jumps over the lazy dog" is a pangram, 
+ * because it uses the letters A-Z at least once (case is irrelevant).
+  Given a string, detect whether or not it is a pangram. 
+  Return: True if it is, 
+          False if not. 
+          Ignore numbers and punctuation.
+ */
+
+function isPangram(str) {
+  const sentence = str.toLowerCase().replace(/[^a-z]/g, "");
+  console.log(sentence);
+  const lettersCounter = {};
+
+  for (let letter of sentence) {
+    lettersCounter[letter] = (lettersCounter[letter] || 0) + 1;
+  }
+  const letterCounterLength = Object.keys(lettersCounter).length;
+  return letterCounterLength === 26;
+}
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog")); // ==> true
+console.log(isPangram("The quick brown fox jumps over the lazy do")); // ==> false
+console.log(isPangram("The quick")); // ==> false
