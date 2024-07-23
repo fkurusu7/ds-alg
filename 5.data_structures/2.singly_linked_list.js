c5371c0cb1ea1e04b86051f89f3183015c19a01d
fkurusu7 <f.douzez@gmail.com>
2024-07-22 23:46:26 -0600

    add insert and remove methods to singly linked list

--- 5.data_structures/2.singly_linked_list.js
+++ 5.data_structures/2.singly_linked_list.js
@@ -138,36 +138,133 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  /** PSEUDOCODE for SETTING a value on an specific Node
+    - This function should accept an index and a value
+    - Use the get(i) to find the specific node 
+    - If the node is not found, return false
+    - if the node is found, set the value of that node to be 
+      the value passed to the function and return true.
+  */
+  set(index, value) {
+    const node = this.get(index);
+    if (!node) return false;
+
+    node.val = value;
+    return true;
+  }
+
+  /** Insert. Adding a node to the linked 
+      list at a specidig position
+   * PSEUDOCODE:
+    - If the index is less than zero 
+      or greater than the length, return false
+    - if the index is the same as the length, push a new node
+      to the end of the list... use PUSH
+    - if the index is 0, unshift a new node.
+
+    - Otherwise, get the node at the previous current index
+      ==> index - 1 
+    - Set the next property on that node (previous) to be the new node
+    - Set the next property on the new node to be the previous next
+    - Increment the length
+    - Return true  
+  */
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+
+    // UNSHIFT & PUSH increment this.length
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.length) return !!this.push(value);
+
+    const previousNode = this.get(index - 1);
+    const newNode = new Node(value);
+    newNode.next = previousNode.next;
+    previousNode.next = newNode;
+    this.length++;
+    return true;
+  }
+
+  /** Removing a node from the list at a specific position
+   * PSEUDOCODE:
+    - If the index is less than zero 
+      or greater than the length return null
+    - If the index is the same as the length - 1, pop()
+    - if the index is 0, shift()
+    - Otherwise, get the node with index = index - 1
+    - Set the next property on that node to be the next of the next node
+    - Decrement the length
+    - Return the value of the removed node
+   */
+  remove(index) {
+    if (index < 0 || index >= this.length) return null;
+
+    // POP & SHIFT
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const previousNode = this.get(index - 1);
+    const removedNode = previousNode.next;
+    previousNode.next = removedNode.next;
+    this.length--;
+    return removedNode;
+  }
 }
 
-const listPush = new SinglyLinkedList();
-listPush.push(3);
-console.log(listPush);
-listPush.push(5);
-console.log(listPush);
-listPush.push(1);
-console.log(listPush);
-listPush.push(10);
-console.log(listPush);
+const linkedList = new SinglyLinkedList();
+linkedList.push(3);
+console.log(linkedList);
+linkedList.push(5);
+console.log(linkedList);
+linkedList.push(1);
+console.log(linkedList);
+linkedList.push(10);
+console.log(linkedList);
 
 const listPop = new SinglyLinkedList();
 console.log(listPop.pop());
-console.log(listPush.pop());
-console.log(listPush);
-// console.log(listPush.pop());
-// console.log(listPush);
-// console.log(listPush.pop());
-// console.log(listPush);
-// console.log(listPush.pop());
-// console.log(listPush);
-// console.log(listPush.pop());
+console.log(linkedList.pop());
+console.log(linkedList);
+// console.log(linkedList.pop());
+// console.log(linkedList);
+// console.log(linkedList.pop());
+// console.log(linkedList);
+// console.log(linkedList.pop());
+// console.log(linkedList);
+// console.log(linkedList.pop());
 
-console.log(listPush);
-console.log(listPush.shift());
-console.log(listPush);
+console.log(linkedList);
+console.log(linkedList.shift());
+console.log(linkedList);
 
-console.log(listPush.unshift(99));
-console.log(listPush.unshift("ONE"));
+console.log(linkedList.unshift(99));
+console.log(linkedList.unshift("ONE"));
 
-console.log(listPush);
-console.log(listPush.get(3));
+console.log(linkedList);
+console.log(linkedList.get(3));
+console.log(linkedList.set(3, "SEEEETTT"));
+console.log(linkedList);
+console.log(linkedList.set(1, "setty"));
+console.log(linkedList);
+console.log(linkedList.set(7, "setty"));
+console.log(linkedList.set(-1, "setty"));
+
+console.log("*********************************************************");
+console.log("*********************************************************");
+console.log("*********************************************************");
+
+console.log(linkedList);
+console.log(linkedList.insert(1, "Two"));
+console.log(linkedList.insert(-1, "Two"));
+console.log(linkedList.insert(9, "Two"));
+console.log(linkedList.insert(0, "zero"));
+console.log(linkedList.insert(linkedList.length, "last"));
+console.log(linkedList);
+console.log(linkedList.pop());
+console.log(linkedList.pop());
+console.log(linkedList);
+
+console.log(linkedList.remove(5));
+console.log(linkedList.remove(3));
+console.log(linkedList.remove(-1));
+console.log(linkedList);
