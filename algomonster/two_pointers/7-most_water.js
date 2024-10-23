78629ea89ed2cb864f0462a926d8fb1cf9483be9
fkurusu7 <coding.fcv@gmail.com>
2024-10-22 23:25:24 -0600

    add sliding window examples

--- algomonster/two_pointers/7-most_water.js
+++ algomonster/two_pointers/7-most_water.js
@@ -5,6 +5,9 @@
 
   Input: [1,8,6,2,5,4,8,3,7].
   Output: 49.
+
+  Your function should calculate the maximum area that can 
+  be trapped by two lines.
 */
 
 function containerWithMostWater(arr) {
@@ -12,16 +15,17 @@ function containerWithMostWater(arr) {
   let right = arr.length - 1;
   let max = 0;
 
-  while (left < right) {
+  while (left <= right) {
     const currentArea = Math.min(arr[left], arr[right]) * (right - left);
     max = Math.max(max, currentArea);
 
-    if (arr[left] < arr[right]) {
+    if (arr[left] <= arr[right]) {
       left++;
     } else {
       right--;
     }
   }
+
   return max;
 }
 
